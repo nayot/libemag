c0770a7fee69b34b535f86ef87fa838cc4cb1f8d
Nayot Kurukitkoson <nayot@eng.buu.ac.th>
2024-12-20 23:30:01 +0700

    Make installable package

---
+++ libemag/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="libemag",
+    version="0.1.0",
+    packages=find_packages(),
+    install_requires=[],
+    description="Package providing functions used in Engineering Electromagnetic Course at BUU",
+    author="Dr Nayot Kurukitkoson",
+    author_email="nayot@eng.buu.ac.th",
+    url="https://www.eng.buu.ac.th",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
+
